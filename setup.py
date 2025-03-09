fff774860267a40893c5c28834c3950771e708cd
Ahmed Ibrahim <ahmeddark369@gmail.com>
2025-03-09 21:12:28 +0200

    standardize build process, remove wheel, add build

--- setup.py
+++ setup.py
@@ -2,15 +2,4 @@ from setuptools import setup
 
 setup(
     name='docugen',
-    license='MIT',
-    version='0.0.1',
-    description='A powerful Python documentation generator that swoops in to automatically create and maintain clean, comprehensive docstrings for your codebase',
-    entry_points={
-        'console_scripts': [
-            'docugen = docugen.cli:main',
-        ],
-        'pre-commit': [
-            'docugen = docugen.cli:main',
-        ],
-    },
 )
